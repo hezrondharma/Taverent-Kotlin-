e0d90c90b0f028f82a9a0393ec0538257125ace9
Hezron :) <hezrondharma@gmail.com>
2022-11-28 18:10:16 +0700

    push

---
+++ app/src/main/java/com/example/taverent/SewaMain.kt
@@ -0,0 +1,11 @@
+package com.example.taverent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SewaMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sewa_main)
+    }
+}
