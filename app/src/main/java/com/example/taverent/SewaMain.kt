7534a735b1841abfc95c81845350ad61ebbb82ce
Hezron :) <hezrondharma@gmail.com>
2022-11-29 23:37:48 +0700

    update penyewa

--- app/src/main/java/com/example/taverent/SewaMain.kt
+++ app/src/main/java/com/example/taverent/SewaMain.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.ConditionVariable
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.taverent.databinding.ActivitySewaMainBinding
 
 class SewaMain : AppCompatActivity() {
@@ -15,34 +16,34 @@ class SewaMain : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.btnHomeSewa.setOnClickListener {
-            Toast.makeText(this, "home", Toast.LENGTH_SHORT).show()
             gray(binding)
             binding.imHomeSewa.setImageResource(R.drawable.ic_baseline_home_24)
             binding.txHomeSewa.setTextColor(Color.parseColor("#000000"))
+            changefragment(HomeSewa())
         }
         binding.btnChatSewa.setOnClickListener {
-            Toast.makeText(this, "chat", Toast.LENGTH_SHORT).show()
             gray(binding)
             binding.imChatSewa.setImageResource(R.drawable.ic_baseline_chat_24)
             binding.txChatSewa.setTextColor(Color.parseColor("#000000"))
+            changefragment(ChatSewa())
         }
         binding.btnKelolaSewa.setOnClickListener {
-            Toast.makeText(this, "kelola", Toast.LENGTH_SHORT).show()
             gray(binding)
             binding.imKelolaSewa.setImageResource(R.drawable.ic_baseline_settings_cell_24)
             binding.txKelolaSewa.setTextColor(Color.parseColor("#000000"))
+            changefragment(KelolaSewa())
         }
         binding.btnStatistikSewa.setOnClickListener {
-            Toast.makeText(this, "Statistik", Toast.LENGTH_SHORT).show()
             gray(binding)
             binding.imStatistikSewa.setImageResource(R.drawable.ic_baseline_insert_chart_outlined_24)
             binding.txStatistikSewa.setTextColor(Color.parseColor("#000000"))
+            changefragment(StatistikSewa())
         }
         binding.btnAkunSewa.setOnClickListener {
-            Toast.makeText(this, "Akun", Toast.LENGTH_SHORT).show()
             gray(binding)
             binding.imAkunSewa.setImageResource(R.drawable.ic_baseline_person_24)
             binding.txAkunSewa.setTextColor(Color.parseColor("#000000"))
+            changefragment(AkunSewa())
         }
     }
 
@@ -57,6 +58,12 @@ class SewaMain : AppCompatActivity() {
         bind.txKelolaSewa.setTextColor(Color.parseColor("#C9C9C9"))
         bind.txStatistikSewa.setTextColor(Color.parseColor("#C9C9C9"))
         bind.txAkunSewa.setTextColor(Color.parseColor("#C9C9C9"))
-
+    }
+    fun changefragment (fragments :Fragment){
+        val fragreplace=fragments
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        transaction.replace(R.id.fragmentContainerView3, fragreplace)
+        transaction.commit()
     }
 }
