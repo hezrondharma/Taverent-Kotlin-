dc372213b9667ea770d9055dda232c7d678da115
brawnyoracle <michaelt.gan@gmail.com>
2022-12-21 23:19:45 +0700

    help unfinished

---
+++ Tavarent-Laravel-/Tavarent/resources/views/help.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.layout')
+@section('title','Help Center')
+@section('navbar')
+<div class="navigation-wrap bg-light start-header start-style">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-md navbar-light">
+
+                    <a href="/penyewa"><img src="{{ asset('img/tavarentlogo-removebg-big.png')}}" width="50px" alt=""></a>
+                </nav>
+            </div>
+@endsection
+@section('content')
+<div class="Container"></div>
+@endsection
