edd37bddb767d7616cd3f14c8de588428238a990
brawnyoracle <michaelt.gan@gmail.com>
2022-12-22 18:45:44 +0700

    help

--- Tavarent-Laravel-/Tavarent/resources/views/help.blade.php
+++ Tavarent-Laravel-/Tavarent/resources/views/help.blade.php
@@ -12,5 +12,22 @@
             </div>
 @endsection
 @section('content')
-<div class="Container"></div>
+<div class="Container">
+    <h1>Help Center</h1>
+    <div>
+        <h3>Getting Started</h3>
+        <ul>
+           <a href="">cara membuat akun</a> <br>
+           <a href="">cara membuka chat</a> <br>
+           <a href="">cara mencari tempat penginapan</a> <br>
+           <a href="">cara menyewa tempat</a> <br>
+        </ul>
+    </div>
+    <h3>Account problems</h3>
+    <ul>
+       <a href="">akun saya dihack?</a> <br>
+       <a href="">kenapa akun saya diban?</a> <br>
+
+    </ul>
+</div>
 @endsection
